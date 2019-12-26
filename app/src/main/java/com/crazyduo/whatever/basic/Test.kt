cc73bdb167b246da5803cd9ec821d4ca445ece08
hashtag <hashtag>
2019-12-26 18:36:24 +0800

    add basic syntax

---
+++ app/src/main/java/com/crazyduo/whatever/basic/Test.kt
@@ -0,0 +1,8 @@
+package com.crazyduo.whatever.basic
+
+/**
+ * Created by ruandong on 2019/12/26.
+ */
+fun main() {
+ println("jjj")
+ }
---
+++ app/src/main/java/kotlinpractice/basic/BasicSyntax.kt
@@ -0,0 +1,130 @@
+/**
+ * Created by ruandong on 2019/12/26.
+ * https://www.kotlincn.net/docs/reference/basic-syntax.html
+ * https://huanglizhuo.gitbooks.io/kotlin-in-chinese/content/GettingStarted/Basic-Syntax.html
+ */
+
+
+fun printSum(a: Int, b: Int): Unit {
+    println("sum of $a and $b is ${a + b}")
+}
+
+
+fun stringTamplate() {
+
+    var a = 1
+// simple name in template:
+    val s1 = "a is $a"
+
+    a = 2
+// arbitrary expression in template:
+    val s2 = "${s1.replace("is", "was")}, but now is $a"
+
+    print(s2)
+}
+
+
+fun maxOf(a: Int, b: Int) = if (a > b) a else b
+
+
+fun parseInt(str: String): Int? {
+    // ...
+
+    return str.toInt()
+}
+
+fun getStringLegth(obj: Any): Int? {
+
+
+    if (obj is String) {
+        return obj.length
+    }
+
+
+    return null
+}
+
+fun loop() {
+    val items = listOf("apple", "banana", "kiwifruit", 1, false, null)
+    for (item in items) {
+        println(item)
+    }
+
+
+}
+
+fun whenTest(obj: Any) = when (obj) {
+    1 -> "one"
+
+    2 -> ""
+
+    "ss" -> "str"
+
+    else -> null
+}
+
+fun range() {
+    val x = 9
+    val y = 10
+
+    if (x in 1..y) {
+        print("x in 1 - y")
+        return
+    }
+    print("x not in 1 - y")
+}
+
+fun controlFlow() {
+
+
+    for (i in 1..6) {
+        print(i)
+
+    }
+
+    for (i in 10 downTo 1 step 2) {
+        println(i)
+    }
+
+
+    val items = listOf("a", "b", "c", "c")
+
+
+    for (item in items) {
+        println(item)
+    }
+}
+
+fun filter() {
+
+    val fruits = listOf("apple", "ms", "google", "fb", "amazon")
+
+    fruits
+            .filter { it.startsWith("a") }
+            .sortedBy { it }
+            .map { it.toUpperCase() }
+            .forEach { println(it) }
+}
+
+
+
+fun main() {
+    filter()
+//    controlFlow()
+//    range()
+//    var x:Any = 0
+//    val b = x is String
+//    print(b )
+
+//    print(whenTest("ss"))
+//    loop()
+//    val x = 9
+//    print("x is $x")
+//    print(getStringLegth("iaf"))
+//    print("test")
+//    printSum(3, 4)
+//    stringTamplate()
+//    print(maxOf(2, 2))
+//    print(parseInt("033"))
+}
+
