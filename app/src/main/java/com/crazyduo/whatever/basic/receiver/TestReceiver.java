8861b8382a84736d9bf31604a2f268242776ec84
crazydudo <>
2022-06-27 15:12:43 +0800

    feat: add broadcast-隐式转显示

--- app/src/main/java/com/crazyduo/whatever/basic/receiver/TestReceiver.java
+++ app/src/main/java/com/crazyduo/whatever/basic/receiver/TestReceiver.java
@@ -4,11 +4,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
 public class TestReceiver extends BroadcastReceiver {
     private static final String TAG="TestReceiver";
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.e(TAG, "收到广播action=" + intent.getAction());
+        Log.e(TAG, "收到广播action======" + intent.getAction());
+        Toast.makeText(context, "收到广播action======" + intent.getAction(), Toast.LENGTH_LONG).show();
     }
 }
--- clientapp/src/main/java/com/example/clientapp/basic/BroadcastActivity.java
+++ clientapp/src/main/java/com/example/clientapp/basic/BroadcastActivity.java
@@ -2,11 +2,14 @@ package com.example.clientapp.basic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.clientapp.R;
 
@@ -32,18 +35,39 @@ public class BroadcastActivity extends AppCompatActivity {
                 Intent intent = new Intent();
                 intent.setAction(TEST_ACTION);
                 //指定接收者包名
-                intent.setPackage("com.crazyduo.whatever");
+//                intent.setPackage("com.crazyduo.whatever");
+                //接收者app停止中也可以收到
+                intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
 
-                List<ResolveInfo> resolveInfos = getPackageManager().queryBroadcastReceivers(intent, 0);
-                Log.e(TAG, "onClick:size()========" + resolveInfos.size());
-                Log.e(TAG, "onClick:getAction()========" + intent.getAction());
-                for (int i = 0; i < resolveInfos.size(); i++) {
-                    Log.e(TAG, "onClick:packageName======== " + resolveInfos.get(i).activityInfo.packageName);
-                }
+//                List<ResolveInfo> resolveInfos = getPackageManager().queryBroadcastReceivers(intent, 0);
+//                Log.e(TAG, "onClick:size()========" + resolveInfos.size());
+//                Log.e(TAG, "onClick:getAction()========" + intent.getAction());
+//                for (int i = 0; i < resolveInfos.size(); i++) {
+//                    Log.e(TAG, "onClick:packageName======== " + resolveInfos.get(i).activityInfo.packageName);
+//                    intent.setPackage(resolveInfos.get(i).activityInfo.packageName);
+//                    sendBroadcast(intent);
+//                }
 
-                sendBroadcast(intent);
+                sendImplicitBroadcast(BroadcastActivity.this,intent);
             }
         });
+    }
+
+    private void sendImplicitBroadcast(Context context, Intent i) {
+        PackageManager pm = context.getPackageManager();
+        List< ResolveInfo> matches = pm.queryBroadcastReceivers(i, 0);
+
+        if (matches==null || matches.size()==0) {
+            Toast.makeText(this, "未查到广播接收者", Toast.LENGTH_SHORT).show();
+            Log.e(TAG, "查询公钥上送结果广播接受者失败");
+        }
+        for (ResolveInfo resolveInfo : matches) {
+            Intent intent = new Intent(i);
+            intent.setPackage(resolveInfo.activityInfo.applicationInfo.packageName);
+//            intent.setAction("com.android.factorymode.action.SECRET_KEY_UPLOADED");
+            context.sendBroadcast(intent);
+            Log.e(TAG, "handleUploadResult---> sendBroadcast action = " + intent.getAction());
+        }
     }
 
 }
